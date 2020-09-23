e9dcc63d0313db142e4e0d0c77f14242cfb88820
Subroto Mohonto <subroto.mohonto@gmail.com>
2020-09-23 17:29:31 +0600

    first commit

--- FormUIDesignIOS/ViewController.swift
+++ FormUIDesignIOS/ViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import Toast_Swift
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var teacherNameTF: UITextField!
+    @IBOutlet weak var designationTF: UITextField!
+    @IBOutlet weak var departmentTF: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func saveBtn(_ sender: UIButton) {
+        if teacherNameTF.text!.isEmpty{
+            self.view.makeToast("Please, Enter Teacher Name",duration : 3.0, position:.bottom, title: nil)
+        } else if designationTF.text!.isEmpty{
+            self.view.makeToast("Please, Enter Designation",duration : 3.0, position:.bottom, title: nil)
+        }else if departmentTF.text!.isEmpty{
+            self.view.makeToast("Please, Enter Department",duration : 3.0, position:.bottom, title: nil)
+        }else{
+            self.view.makeToast("Save Successfully",duration : 3.0, position:.bottom, title: nil)
+        }
+    }
+    
 }
 
---
+++ Pods/Target Support Files/Pods-FormUIDesignIOS/Pods-FormUIDesignIOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FormUIDesignIOS : NSObject
+@end
+@implementation PodsDummy_Pods_FormUIDesignIOS
+@end
